accc73ad42d427765adcace731924f125f9bd70b
clarkk <clark@electrobeat.dk>
2025-03-26 23:37:02 +0000

    limit

--- limit.go
+++ limit.go
@@ -1,7 +1,5 @@
 package api
 
-//import "math"
-
 type Limit struct {
 	Offset		uint32		`json:"offset"`
 	Limit		uint8		`json:"limit"`
@@ -10,11 +8,6 @@ type Limit struct {
 
 func (l *Limit) Limit_max(max uint8){
 	l.Limit = min(l.Limit, max)
-	//	Adjust relationship between offset and limit (pagination)
-	/*if l.Offset != 0 {
-		f := float64(l.Offset) / float64(l.Limit)
-		l.Offset = uint32(math.Round(f)) * uint32(l.Limit)
-	}*/
 }
 
 func (l *Limit) Count(count uint32) bool {
@@ -22,9 +15,6 @@ func (l *Limit) Count(count uint32) bool {
 	//	Offset out of range
 	if l.Offset != 0 && l.Offset + 1 > count {
 		return false
-		//	Adjust offset to last page
-		/*f := float64(l.Entries) / float64(l.Limit)
-		l.Offset = uint32(math.Floor(f)) * uint32(l.Limit)*/
 	}
 	return true
 }
