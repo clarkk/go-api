4dd8299a3e27ecf651a1bc80515afa7162dc8ad6
clarkk <clark@electrobeat.dk>
2025-04-19 10:53:48 +0000

    errin has()

--- errin/errors.go
+++ errin/errors.go
@@ -20,9 +20,19 @@ func (m *Map) Set(key, msg string){
 	(*m)[key] = errors.New(msg)
 }
 
+func (m Map) Has(key string) bool {
+	_, ok := m[key]
+	return ok
+}
+
 func (m *Map_lang) Set(key string, lang *Lang){
 	if *m == nil {
 		*m = Map_lang{}
 	}
 	(*m)[key] = lang
+}
+
+func (m Map_lang) Has(key string) bool {
+	_, ok := m[key]
+	return ok
 }
