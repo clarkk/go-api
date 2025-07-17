5066696f06613ee6f55f3c757c846331114d2827
clarkk <clark@electrobeat.dk>
2025-07-17 14:03:24 +0000

    update

--- response.go
+++ response.go
@@ -80,14 +80,22 @@ func (a *Request) Error(code int, err error){
 //	Error JSON response and log unexpected error
 func (a *Request) Error_log(code int, err error, e *env.Environment){
 	a.Error(code, nil)
-	url := a.r.Host+a.r.URL.Path
 	var env_string string
 	if e == nil {
 		env_string = "<nil>"
 	} else {
 		env_string = fmt.Sprintf("%v", e.Data())
 	}
-	log.Printf("HTTP %d: %s %s %s\nENV: %s\nPOST: %s\nERROR: %s", code, a.r.Method, url, a.r.URL.RawQuery, env_string, string(a.body_received), errors.Wrap(err, 2).ErrorStack())
+	log.Printf("HTTP %d: %s %s %s\nEnv: %s\nPost payload (%d bytes): %s\nError: %s",
+		code,
+		a.r.Method,
+		a.r.Host+a.r.URL.Path,
+		a.r.URL.RawQuery,
+		env_string,
+		len(a.body_received),
+		string(a.body_received),
+		errors.Wrap(err, 2).ErrorStack(),
+	)
 }
 
 //	Errors JSON response
