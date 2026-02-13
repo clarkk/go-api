70f3ca1ca7ca6c674bf9ee545206661740fdce7d
clarkk <clark@electrobeat.dk>
2026-02-13 18:36:04 +0000

    udpate

--- errin/errors.go
+++ errin/errors.go
@@ -6,8 +6,8 @@ import (
 )
 
 type (
-	Map			list[string]
-	Map_lang	list[*Lang]
+	Map			[]item[string]
+	Map_lang	[]item[*Lang]
 	
 	Lang struct {
 		Key		string
@@ -15,15 +15,59 @@ type (
 	}
 	Rep			map[string]any
 	
-	list[T any]	[]item[T]
-	
 	item[T any] struct {
 		key		string
 		value	T
 	}
 )
 
-func (l *list[T]) Set(key string, value T){
+func (m *Map) Set(key, value string) {
+	set(m, key, value)
+}
+
+func (m Map) Has(key string) bool {
+	return has(m, key)
+}
+
+func (m Map) Output() *map_json.Map {
+	return output(m)
+}
+
+func (m Map) String() string {
+	if len(m) == 0 {
+		return ""
+	}
+	s := make([]string, len(m))
+	for i, v := range m {
+		s[i] = v.key+": "+v.value
+	}
+	return strings.Join(s, ", ")
+}
+
+func (m *Map_lang) Set(key string, value *Lang) {
+	set(m, key, value)
+}
+
+func (m Map_lang) Has(key string) bool {
+	return has(m, key)
+}
+
+func (m Map_lang) Output() *map_json.Map {
+	return output(m)
+}
+
+func (m Map_lang) String() string {
+	if len(m) == 0 {
+		return ""
+	}
+	s := make([]string, len(m))
+	for i, v := range m {
+		s[i] = v.key+": "+v.value.Key
+	}
+	return strings.Join(s, ", ")
+}
+
+func set[T any](l *[]item[T], key string, value T) {
 	for i := range *l {
 		if (*l)[i].key == key {
 			(*l)[i].value = value
@@ -36,7 +80,7 @@ func (l *list[T]) Set(key string, value T){
 	})
 }
 
-func (l list[T]) Has(key string) bool {
+func has[T any](l []item[T], key string) bool {
 	for _, v := range l {
 		if v.key == key {
 			return true
@@ -45,7 +89,7 @@ func (l list[T]) Has(key string) bool {
 	return false
 }
 
-func (l list[T]) Output() *map_json.Map {
+func output[T any](l []item[T]) *map_json.Map {
 	if len(l) == 0 {
 		return nil
 	}
@@ -54,26 +98,4 @@ func (l list[T]) Output() *map_json.Map {
 		m.Set(v.key, v.value)
 	}
 	return m
-}
-
-func (m Map) String() string {
-	if m == nil || len(m) == 0 {
-		return ""
-	}
-	s := make([]string, len(m))
-	for k, v := range m {
-		s[k] = v.key+": "+v.value
-	}
-	return strings.Join(s, ", ")
-}
-
-func (m Map_lang) String() string {
-	if m == nil || len(m) == 0 {
-		return ""
-	}
-	s := make([]string, len(m))
-	for k, v := range m {
-		s[k] = v.key+": "+v.value.Key
-	}
-	return strings.Join(s, ", ")
 }
