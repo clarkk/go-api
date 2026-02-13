16eeeb0aa9588b0fde35a68e5eb886f87d010d58
clarkk <clark@electrobeat.dk>
2026-02-13 14:51:06 +0000

    update

--- errin/errors.go
+++ errin/errors.go
@@ -7,11 +7,11 @@ import (
 
 type (
 	Map struct {
-		*map_json.Map
+		base
 	}
-
+	
 	Map_lang struct {
-		*map_json.Map
+		base
 	}
 	
 	Lang struct {
@@ -19,60 +19,60 @@ type (
 		Replace	Rep
 	}
 	Rep			map[string]any
+	
+	base struct {
+		*map_json.Map
+	}
 )
 
-func (m *Map) Set(key, msg string){
+func (b base) Empty() bool {
+	return b.Map == nil || b.Len() == 0
+}
+
+func (b base) Has(key string) bool {
+	if b.Map == nil {
+		return false
+	}
+	_, ok := b.Get(key)
+	return ok
+}
+
+func (m *Map) Set(key, msg string) {
 	if m.Map == nil {
 		m.Map = map_json.New()
 	}
 	m.Map.Set(key, msg)
 }
 
-func (m Map) Has(key string) bool {
-	if m.Map == nil {
-		return false
-	}
-	_, ok := m.Map.Get(key)
-	return ok
-}
-
-func (m *Map_lang) Set(key string, lang *Lang){
+func (m *Map_lang) Set(key string, lang *Lang) {
 	if m.Map == nil {
 		m.Map = map_json.New()
 	}
 	m.Map.Set(key, lang)
 }
 
-func (m Map_lang) Has(key string) bool {
-	if m.Map == nil {
-		return false
-	}
-	_, ok := m.Map.Get(key)
-	return ok
-}
-
-func (m *Map_lang) String() string {
-	if m == nil || m.Map == nil {
+func (m Map_lang) String() string {
+	if m.Empty() {
 		return ""
 	}
-	keys := m.Map.Keys()
+	keys := m.Keys()
 	s := make([]string, len(keys))
 	for i, k := range keys {
-		val, _ := m.Map.Get(k)
-		s[i] = k+": "+val.(*Lang).Key
+		val, _ := m.Get(k)
+		s[i] = k + ": " + val.(*Lang).Key
 	}
 	return strings.Join(s, ", ")
 }
 
-func (m *Map) String() string {
-	if m == nil || m.Map == nil {
+func (m Map) String() string {
+	if m.Empty() {
 		return ""
 	}
-	keys := m.Map.Keys()
+	keys := m.Keys()
 	s := make([]string, len(keys))
 	for i, k := range keys {
-		val, _ := m.Map.Get(k)
-		s[i] = k+": "+val.(string)
+		val, _ := m.Get(k)
+		s[i] = k + ": " + val.(string)
 	}
 	return strings.Join(s, ", ")
 }
